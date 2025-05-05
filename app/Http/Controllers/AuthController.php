5aec73b1bb6c8be7c1358cf31eccdc1281e5566c
ACTSoloDev <fierce.tio@student.passerellesnumeriques.org>
2025-05-04 22:32:39 -0700

    late submission

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('dashboard');
+            return redirect()->intended('products');
         }
 
         return back()->withErrors([
@@ -58,7 +58,8 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('login');
     }
+    
 }
 
